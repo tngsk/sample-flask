8043fe6f27ef2cb6a9119d80bd80b91a94ea44eb
nagasaki <>
2022-12-06 12:53:38 +0900

    flask-adminの準備（のみ）

--- app.py
+++ app.py
@@ -2,8 +2,33 @@ import os
 import sqlite3
 
 from flask import Flask, g, render_template, request
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+# app.config['SECRET_KEY'] = 'secret_key'
+# db = SQLAlchemy(app)
+
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(30),unique=True)
+
+# #DBのクリエイト宣言
+# db.create_all()
+
+# #DBが空の状態(最初の1回)はtestuserを作成する
+# user = User.query.filter_by(username='testuser').first()
+# if user is None:
+#     testuser = User(username='testuser')
+#     db.session.add(testuser)
+#     db.session.commit()
+
+# admin = Admin(app)
+# admin.add_view(ModelView(User, db.session))
+
+
 
 DEBUG = os.environ.get("DEBUG")
 
@@ -16,8 +41,10 @@ def get_db():
 
 @app.route('/')
 def index():
+    # user = User.query.filter_by(username='testuser').first()
+    # name = user.username
     name = "You"
-    title = "Flask Test"
+    title = "Hello from app"
     response = render_template(
         "index.html",
         title=title,
