6fae3a996b5318a6481a3300e2a144c718399f4a
nagasaki <>
2022-12-06 11:27:25 +0900

    sqliteに変更

--- app.py
+++ app.py
@@ -1,36 +1,17 @@
 import os
+import sqlite3
 
-import pymysql
-from flask import Flask, render_template
+from flask import Flask, g, render_template
 
 app = Flask(__name__)
 
 DEBUG = os.environ.get("DEBUG")
 
 # db setting
-
-db = None
-
-if DEBUG:
-    db = pymysql.connect(
-        host='localhost',
-        port=3306,
-        user='root',
-        password='root',
-        db='testdb',
-        charset='utf8',
-        cursorclass=pymysql.cursors.DictCursor,
-    )
-
-# db access
-
-# cur = db.cursor()
-# sql = "select * from members"
-# cur.execute(sql)
-# members = cur.fetchall()
-
-# cur.close()
-# db.close()
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect("db.sqlite")
+    return g.db
 
 @app.route('/')
 def index():
