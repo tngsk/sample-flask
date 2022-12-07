514de1d71d78c7b3eb98f8d3a417d437c7a5fbed
nagasaki tomohiro <nagasaki@tohtech.ac.jp>
2022-12-07 18:59:15 +0900

    sqliteでの登録と検索部分を作成

--- app.py
+++ app.py
@@ -2,33 +2,8 @@ import os
 import sqlite3
 
 from flask import Flask, g, render_template, request
-from flask_admin import Admin
-from flask_admin.contrib.sqla import ModelView
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-# app.config['SECRET_KEY'] = 'secret_key'
-# db = SQLAlchemy(app)
-
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(30),unique=True)
-
-# #DBのクリエイト宣言
-# db.create_all()
-
-# #DBが空の状態(最初の1回)はtestuserを作成する
-# user = User.query.filter_by(username='testuser').first()
-# if user is None:
-#     testuser = User(username='testuser')
-#     db.session.add(testuser)
-#     db.session.commit()
-
-# admin = Admin(app)
-# admin.add_view(ModelView(User, db.session))
-
-
 
 DEBUG = os.environ.get("DEBUG")
 
@@ -41,14 +16,20 @@ def get_db():
 
 @app.route('/')
 def index():
-    # user = User.query.filter_by(username='testuser').first()
-    # name = user.username
+
+    db = get_db()
+
+    cursor = db.execute("select * from データリスト order by コード")
+    data = cursor.fetchall()
+    db.close()
+
     name = "You"
     title = "Hello from app"
     response = render_template(
         "index.html",
         title=title,
-        name=name)
+        name=name,
+        data=data)
     return response
 
 
@@ -63,17 +44,39 @@ def urlparam(data=None):
 
 @app.route('/post/',methods=["POST"])
 def post():
-    user_id = request.form["user_id"]
-    post_data = request.form["post_data"]
+    user_id = request.form["userid"]
+    post_data = request.form["text"]
 
     db = get_db()
+    cursor = db.execute("select MAX(コード) AS max_code from データリスト")
+    for row in cursor:
+        new_code = row[0] + 1
+    cursor.close()
+
+    sql = "INSERT INTO データリスト(コード, テキスト, 値) values ({}, '{}', {})".format(new_code, post_data, 100)
+    db.execute(sql)
+    db.commit()
+
+    cursor = db.execute("select * from データリスト order by コード")
+    data = cursor.fetchall()
+    db.close()
 
     response = render_template(
         "index.html",
-        data=post_data,
+        name=user_id,
+        data=data,
     )
     return response
 
+
+@app.route('/form/')
+def form():
+    db = get_db()
+
+    response = render_template(
+        "form.html",
+    )
+    return response
 
 @app.route('/init_table/')
 def init_table():
